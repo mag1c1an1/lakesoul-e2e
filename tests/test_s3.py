3ada16a65583638ff561183ea1a1b9159c9a2e94
mag1c1an1 <mag1cian@icloud.com>
2025-07-08 02:18:39 +0800

    add some tasks

--- tests/test_s3.py
+++ tests/test_s3.py
@@ -1,8 +1,10 @@
 # SPDX-FileCopyrightText: LakeSoul Contributors
-# 
+#
 # SPDX-License-Identifier: Apache-2.0
 
-from e2etest.core import E2E_DATA_DIR, s3_delete_dir, s3_delete_jars, s3_upload_jars
+
+from e2etest.s3 import s3_delete_dir, s3_delete_jars, s3_upload_jars
+from e2etest.vars import E2E_DATA_DIR
 
 
 def test_s3():
